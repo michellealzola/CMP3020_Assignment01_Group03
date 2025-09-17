9545ac673abfdf46191765c45a3a6af23497eee9
michellealzola <michelle.alzola@yahoo.ca>
2025-09-17 11:33:59 -0600

    MainWindow Class

---
+++ MainWindow.py
@@ -0,0 +1,9 @@
+from PySide6.QtWidgets import (
+    QMainWindow, QApplication, QVBoxLayout,
+    QPushButton, QWidget, QHBoxLayout,
+)
+
+class MainWindow(QMainWindow):
+    def __init__(self, /):
+        super().__init__()
+
